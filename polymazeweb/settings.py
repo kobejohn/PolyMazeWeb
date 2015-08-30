d756066a6c6031a0fbe09e5c92df35be778a7c9b
KobeJohn <niericentral@gmail.com>
2015-08-30 21:11:36 +0900

    change database to postgresql

--- polymazeweb/settings.py
+++ polymazeweb/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
+from settings_sensitive import *
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -72,14 +74,7 @@ WSGI_APPLICATION = 'polymazeweb.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# from sensitive setup file
 
 
 # Internationalization
